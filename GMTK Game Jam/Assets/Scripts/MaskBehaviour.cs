4e12126b3df8c1bb3f6997c4125749eda181e083
Portal <57550537+Theportalmasters@users.noreply.github.com>
2020-07-10 22:24:34 -0500

    blop
    little intro to it

--- GMTK Game Jam/Assets/Scripts/MaskBehaviour.cs
+++ GMTK Game Jam/Assets/Scripts/MaskBehaviour.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using Unity.Mathematics;
+using UnityEngine;
 
 public class MaskBehaviour : MonoBehaviour
 {
     private int life = 100;
+    int size = 0;
     public int maxSize = 10;
     private void Update()
     {
@@ -11,10 +13,12 @@ public class MaskBehaviour : MonoBehaviour
             MaskControler.spriteMasks.Remove(GetComponent<SpriteMask>());
             Destroy(gameObject);
         }
-        float lifeSquared = Mathf.Clamp(life * life,0,maxSize);
+        float lifeSquared = Mathf.Clamp(life * life,0,size);
 
         transform.localScale = Vector3.one * lifeSquared;
         Degradation.Percent += .001f;
         life--;
+        size++;
+        size = Mathf.Clamp(size,0,maxSize);
     }
 }
