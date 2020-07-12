09f77bc2cb4fd574fef4164f44ba5dd3b6162fa8
Portal <57550537+Theportalmasters@users.noreply.github.com>
2020-07-11 22:25:22 -0500

    No GUI Layer
    Why were we doing that in the first place?

--- GMTK Game Jam/Assets/Scripts/ButtonScript.cs
+++ GMTK Game Jam/Assets/Scripts/ButtonScript.cs
@@ -3,23 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
-    public Transform mask;
     public Transform spriteMask;
     public Transform start;
     public Transform exit;
-    Vector2 startPos;
-    Vector2 exitPos;
-    public void Start()
-    {
-        startPos = start.position;
-        exitPos = exit.position;
-    }
+
     public void Update()
     {
-        mask.position = Input.mousePosition;
         spriteMask.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10));
-        start.position = startPos;
-        exit.position = exitPos;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out RaycastHit hit) && Input.GetMouseButtonDown(0))
+        {
+            if(hit.transform == start)
+            {
+                StartGame();
+            }
+            else if(hit.transform == exit)
+            {
+                Quit();
+            }
+        }
     }
     public void StartGame()
     {
