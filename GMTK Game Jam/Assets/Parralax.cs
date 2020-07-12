027547c19a9cf78e717095f5988e68a2ff152e42
Portal <57550537+Theportalmasters@users.noreply.github.com>
2020-07-12 01:02:44 -0500

    Paralax backround

---
+++ GMTK Game Jam/Assets/Parralax.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parralax : MonoBehaviour
+{
+    public Transform player;
+    Vector3 prevPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position - prevPos * .01f;
+        prevPos = player.position;
+    }
+}
